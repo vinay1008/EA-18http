fbe5c1d0b1d442d12afbe26b8fcd1bdca958d9b9
vinay1008 <vinaypatil1718@gmail.com>
2023-06-02 11:18:38 +0530

    third component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OneComponent } from './one/one.component';
 import { TwoComponent } from './two/two.component';
+import { ThreeComponent } from './three/three.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OneComponent,
-    TwoComponent
+    TwoComponent,
+    ThreeComponent
   ],
   imports: [
     BrowserModule,
